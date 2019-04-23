58120f88bbe80814d66c7609b5f1dad6b3658dc8
juliali8 <31421521+juliali8@users.noreply.github.com>
2019-04-23 17:36:27 -0500

    Add files via upload

---
+++ microsite/finished/script.js
@@ -0,0 +1,7 @@
+function switchWorks(id, id2, id3, id4, id5){
+        document.getElementById(id).style.display = 'block';
+        document.getElementById(id2).style.display = 'none';
+        document.getElementById(id3).style.display = 'none';
+        document.getElementById(id4).style.display = 'none';
+        document.getElementById(id5).style.display = 'none';
+}
